df16952dcbf0c2f4db81538c0d734243278e7a48
ravpratham <ravpratham223@gmail.com>
2023-10-03 09:52:15 +0530

    local changes
    local changes

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 #Q1
-employee = pd.read_csv(r"C:\Users\ravpr\DownloadsEmployee_HR.csv")
-print(employee)
+employee = pd.read_csv(r"Employee_HR.csv")
+print(employee.info())
 print(employee["Department"])
 x=employee.loc[((employee["Department"]=="hr") | (employee["Department"]=="marketing")),"Salary_INR"]
 print(x)
@@ -11,18 +12,19 @@ print(x.std())
 Q3 = np.quantile(x, 0.75)
 Q1 = np.quantile(x, 0.25)
 IQR = Q3 - Q1
-print("IQR",IQR)
+print("Ans 1. IQR",IQR)
 #Q2
 y=employee.loc[employee["Department"]=="hr",'time_spent_company']
-print("range")
-print(y.min(),y.max())
+print("Ans 2. Range", y.min(), y.max())
 
 #Q3
-
 employee.boxplot(by ='Department', column =['time_spent_company'],grid = False,figsize=(10,10))
 
 #Q4
+df = employee.loc[:, employee.columns != 'Department']
+
+maxStd = max(df.std())
 for i in employee.columns:
     if employee[i].dtype !='O':
-        if employee[i].std()==max(employee.std()):
-            print("highest standard deviation",i)
+        if (employee[i].std()==maxStd):
+            print("Ans 4. highest standard deviation",i)
